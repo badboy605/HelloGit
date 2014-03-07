06307e8130d18361f319dc9b3d1c08513d48fed7
Alex Wu <alexyxwu7@gmail.com>
2014-03-08 00:44:29 +1100

    Test push from eclipse

---
+++ HelloWorld/src/com/alex/helloWorld/HelloGit.java
@@ -0,0 +1,9 @@
+package com.alex.helloWorld;
+
+public class HelloGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello Github");
+	}
+}
